0ce7371beade01ceb4bfb12fe41811ec0984f7f9
Mridul Roy <39593927+d4rkr0n1n@users.noreply.github.com>
2023-09-05 16:19:14 +0000

    Updates

--- src/main/java/com/d4rkr0n1n/datetime/DateTime.java
+++ src/main/java/com/d4rkr0n1n/datetime/DateTime.java
@@ -1,17 +1,42 @@
 package com.d4rkr0n1n.datetime;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.ZoneId;
+import java.util.Calendar;
 import java.util.Date;
 
 public class DateTime {
   public static void main(String[] args) {
-    String pattern = "MM-dd-yyyy";
-    SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
-    String date = simpleDateFormat.format(getDate());
-    System.out.println(date);
+    System.out.println(getFormattedDate());
+
+    LocalDate startLocalDate = LocalDate.now();
+    LocalDate endLocalDate = LocalDate.of(2023, Month.DECEMBER, 13);
+    
+    System.out.println(getWeekDifference(startLocalDate, endLocalDate));
   }
 
   private static Date getDate() {
     return new Date();
+  }
+
+  private static String getFormattedDate() {
+    String pattern = "MM-dd-yyyy";
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+    String date = simpleDateFormat.format(getDate());
+    return date;
+  }
+
+  private static int getWeekDifference(LocalDate sDate, LocalDate eDate) {
+    ZoneId zoneId = ZoneId.systemDefault();
+    
+    Calendar startDateCalendar = Calendar.getInstance();
+    startDateCalendar.setTime(Date.from(sDate.atStartOfDay(zoneId).toInstant()));
+    
+    Calendar endDateCalendar = Calendar.getInstance();
+    endDateCalendar.setTime(Date.from(eDate.atStartOfDay(zoneId).toInstant()));
+    
+    return endDateCalendar.get(Calendar.WEEK_OF_YEAR) - startDateCalendar.get(Calendar.WEEK_OF_YEAR);
   }
 }
